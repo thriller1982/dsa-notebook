8602b71b97abfd9289e7f969c6c26e1467b78d5a
Administrator <13576733821@139.com>
2026-03-06 23:40:34 +0800

    几个算法

---
+++ few_algorithms/few_algorithms.c
@@ -0,0 +1,39 @@
+#include "few_algorithms.h"
+
+int BinarySearch(const int arr[], int x, int n) {
+    int mid, high, low;
+    high = n - 1;
+    low = 0;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x > arr[mid]) {
+            low = mid + 1;
+        } else if (x < arr[mid]) {
+            high = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int gcd(int m, int n) {
+    while (n > 0) {
+        int rem = m % n;
+        m = n;
+        n = rem;
+    }
+    return m;
+}
+
+long int pow(long int x, long int n) {
+    if (n == 0) {
+        return 1;
+    }
+    if (n % 2 == 0) {
+        return pow(x * x, n / 2);
+    } else {
+        return pow(x * x, n / 2) * x;
+    }
+}
---
+++ few_algorithms/few_algorithms.h
@@ -0,0 +1,8 @@
+#ifndef DSA_NOTEBOOK_FEW_ALGORITHMS_H
+#define DSA_NOTEBOOK_FEW_ALGORITHMS_H
+
+int BinarySearch(const int arr[], int x, int n);
+int gcd(int m, int n);
+long int pow(long int x, long int n);
+
+#endif //DSA_NOTEBOOK_FEW_ALGORITHMS_H
